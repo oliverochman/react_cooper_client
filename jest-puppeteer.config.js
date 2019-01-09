5bfed5d325c6d26047631987fb171cff58702d2f
oliverochman <oliver.ochman@hotmail.com>
2019-01-09 16:19:08 +0100

    update jest-puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,14 +1,15 @@
 module.exports = {
     launch: {
         headless: false,
-        slowMo: 50,
+        slowMo: 10,
         devtools: true,
         args: ["--no-sandbox", "--disable-popup-blocking", "--disable-infobars"]
     },
     browserContext: 'default',
+
     server: {
-        command: `PORT=3000 react-scripts start`,
+        command: `PORT=3000 BROWSER=none npm run start `,
         port: 3000,
         launchTimeout: 4000,
-    },
+    }
 } 
