a5458f35c830bdc08102c61858e9f506916cdade
oliverochman <oliver.ochman@hotmail.com>
2019-01-10 17:13:41 +0100

    WIP login function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import DisplayCooperResult from './DisplayCooperResult'
 import LoginForm from './LoginForm'
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -18,14 +19,38 @@ class App extends Component {
   }
 
 
-  onLogin(event) {
-    if (this.state.password !== 'password') {
-      this.setState({ loginMessage: "Wrong password", authenticated: false })
-    } else if (this.state.email !== 'johndoe@mail.com') {
-      this.setState({ loginMessage: "Wrong email", authenticated: false })
-    } else {
-      this.setState({ authenticated: true, loginMessage: `Welcome ${this.state.email}` });
-    }
+  onLogin() {
+    debugger;
+    const email = this.state.email;
+    const password = this.state.password;
+    axios({
+      url: "http://localhost:3000/api/v1/auth/sign_in",
+      method: 'POST',
+      data: {
+        email: email,
+        password: password
+      },
+      headers: {
+        'HTTP_ACCEPT': 'application/json'
+      }
+    })
+    .then(function(response) {
+      debugger;
+      console.log(response);
+    })
+    .catch(function (error) {
+      debugger;
+      console.log(error);
+    });
+
+
+    // if (this.state.password !== 'password') {
+    //   this.setState({ loginMessage: "Wrong password", authenticated: false })
+    // } else if (this.state.email !== 'johndoe@mail.com') {
+    //   this.setState({ loginMessage: "Wrong email", authenticated: false })
+    // } else {
+    //   this.setState({ authenticated: true, loginMessage: `Welcome ${this.state.email}` });
+    // }
   }
 
   onChange(event) {
